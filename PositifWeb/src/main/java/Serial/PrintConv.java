0131a4b11f80d3bc354b6a00a86a7841f6273da6
Spherebleue <7spherebleue7@gmail.com>
2018-05-21 15:07:29 +0200

    Add files via upload

---
+++ PositifWeb/src/main/java/Serial/PrintConv.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serial;
+
+import fr.insalyon.dasi.positif.metier.Conversation;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Spherebleue
+ */
+public class PrintConv {
+    public void execute(PrintWriter out, Conversation conv) {
+        
+    }
+}
