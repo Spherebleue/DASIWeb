53c4c2d645cdaa74e344cd730440fe9590e24b2a
Spherebleue <7spherebleue7@gmail.com>
2018-05-21 16:07:08 +0200

    Add files via upload

--- PositifWeb/src/main/java/Serial/PrintConv.java
+++ PositifWeb/src/main/java/Serial/PrintConv.java
@@ -5,8 +5,15 @@
  */
 package Serial;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import fr.insalyon.dasi.positif.metier.Astrologue;
 import fr.insalyon.dasi.positif.metier.Conversation;
+import fr.insalyon.dasi.positif.metier.Medium;
+import fr.insalyon.dasi.positif.metier.Tarologue;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
 /**
  *
@@ -14,6 +21,22 @@ import java.io.PrintWriter;
  */
 public class PrintConv {
     public void execute(PrintWriter out, Conversation conv) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         
+        JsonObject jsonConv = new JsonObject();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date =sdf.format(conv.getHeureDebut());
+        jsonConv.addProperty("HeureDebut",date);
+        date = sdf.format(conv.getHeureFin());
+        jsonConv.addProperty("HeureFin", date);
+        Medium m = conv.getMedium();
+        jsonConv.addProperty("NomMedium",m.getNom());
+        if(m instanceof Astrologue){
+            jsonConv.addProperty("Specialité", "Astrologue");
+        } else if (m instanceof Tarologue) {
+            jsonConv.addProperty("Specialité", "Tarologue");
+        } else {
+            jsonConv.addProperty("Specialité", "Voyant");
+        }
     }
 }
